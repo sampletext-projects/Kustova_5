cf255ff03208a9b78362d81a858da3a9c0d04e41
Bird Egop <sampletext32@bk.ru>
2021-01-31 22:02:03 +0300

    Add project

---
+++ Kustova_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+// ReSharper disable AssignNullToNotNullAttribute
+
+namespace Kustova_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int min = array[0], minIndex = 0;
+            int max = array[0], maxIndex = 0;
+            
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    minIndex = i;
+                }
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }
+            }
+
+            int delta = Math.Abs(maxIndex - minIndex);
+            Console.WriteLine($"Разница индексов: {delta}");
+            Console.ReadKey();
+        }
+    }
+}
